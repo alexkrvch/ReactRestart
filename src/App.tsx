ce0c5ff1b2317d32e2472f7ccbfe975c3210df82
Heikun <heikun@Heikuns-MacBook-Pro.local>
2024-09-22 12:46:03 +0300

    Added classNames function and tested it

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageLazy} from "./pages/AboutPage/AboutPage.lazy";
 import {MainPageLazy} from "./pages/MainPage/MainPage.lazy";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Theme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
