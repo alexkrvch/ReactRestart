d3e3512c6979df3a0b15b9f16cefe0fb5c436a8e
Heikun <heikun@Heikuns-MacBook-Pro.local>
2024-09-24 14:35:40 +0300

    Transfered links to navbar and create component for links and themes for them

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss';
+import {AppLink} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}:NavbarProps) => {
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to={'/'} className={cls.first}>Main</AppLink>
+                <AppLink to={'/about'}>About</AppLink>
+            </div>
+        </div>
+    );
+};
